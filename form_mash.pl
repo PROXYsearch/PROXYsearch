198f2d732a3955ac2e81cf07eac3ef11d19cabcb
mike perez <meekprize@gmail.com>
2014-07-20 23:32:14 -0500

    initial funtionality. returns search results but not unique or without cruft

--- form_mash.pl
+++ form_mash.pl
@@ -4,22 +4,73 @@ use strict;
 use warnings;
 #use diagnostics;
 
+use HTML::TreeBuilder::XPath;
 use LWP::UserAgent;
-use CGI;
+use HTTP::Request::Common qw { POST };
+use CGI ':standard'; 
+#use CGI ':standard', '-debug'; 
 
+my @all_cases;
+
+sub dedup_and_sort_all_cases {
+	return @all_cases;
+}
+
+# processPageContentWide(HTTP::Response response)
+# specific to south carolina site as of July 20 2014
+sub processPageContentWide_treebuilder_xpath {
+	my $response = $_[0];
+	my $tree = HTML::TreeBuilder::XPath->new;
+	my $html = $response->decoded_content;
+	$tree->parse($html);
+
+	my $search_results_html = $tree->findnodes_as_string( '//div[@id="pageContentWide"]//a[@class="bluelink2"]' );
+
+	return qq(<p>$html</p>);
+}
+
+sub processResponse {
+	return processPageContentWide_treebuilder_xpath($_[0]);
+}
+
+my $ua = LWP::UserAgent->new();
 my $cgi = CGI->new();
 my $content = qq(<hr/>);
 
-foreach my $key ( $cgi->param() ) {
-	#my $input{$key} = $cgi->param($key);
-	$content .= qq(<p><h3>$key:</h3> $cgi->param($key)</p>);
+my %form_inputs = (); 
+my $form_action = $cgi->param("form-mash-url[0]");
+my @keys = $cgi->param;
+foreach my $key (@keys) {
+	my $val = $cgi->param($key);
+	$content .= qq(<p>);
+	$content .= qq(<h3>$key</h3><h4>$val</h4>);
+	$form_inputs{$key} = $cgi->param($key);
+	if( $key eq "form-mash-url[0]" ) {
+		#skip first key
+		next;
+	}
+	elsif( $key =~ /^form-mash-url\[\d{1,3}\]$/ ) {
+		if( scalar(keys(%form_inputs)) > 0 ) {
+			#submit to $form_action with current set of form_inputs
+			my $request = POST( $form_action, \%form_inputs);
+			my $response = $ua->request( $request );
+			if( $response->is_error ) {
+				$content .= qq("<small>Error: $form_action<p>$response->status_line</p><p>---begin request---</p><p>$request->as_string()</p><p>---end request---</p></small>);
+			}
+			$content .= processResponse( $response );
+		}
+
+		#reset
+		$form_action = $cgi->param($key);
+		%form_inputs = ();
+
+		$content .= qq(</p>);
+	}
 }
 
 
-my $url = $input{url};
-my $ua = LWP::UserAgent->new();
-my $response = $ua->post( $url, $cgi->param );
-my $content .= $response->decoded_content();
+# $content .= dedup_and_sort_all_cases();
 
 $content .= qq(\n\n);
-print $cgi->header("text/html"), $content;
+print "Content-type: text/html\n\n";
+print $content;
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,58 +5,42 @@ jQuery.ajaxSettings.dataType = 'html';
 jQuery.datepicker.setDefaults({dateFormat:'M d, yy'});
 
 
-/***************************   AJAX Callback functions    **********************************/
-/***************************   AJAX Callback functions    **********************************/
+/**********   AJAX Callback functions    **********************************/
+/**********   AJAX Callback functions    **********************************/
 
-// to be used as a callback for the rails event "ajax:beforeSend"
+// callback for event "ajax:beforeSend"
 var my_ajax_beforeSend = function(target, xhr, settings) {
   console.log("handling ajax:beforeSend");
-
-  var element = jQuery(target);
-  var spinner_text = "Loading...";
-  var opt_spinner_text = element.data("spinner-text");
-  if( opt_spinner_text && opt_spinner_text != "" ) {
-    spinner_text = opt_spinner_text
-  }
-
-  jQuery('#ajax_spinner_modal #spinnertext').html( spinner_text );
-  jQuery('#ajax_spinner_modal').modal({backdrop:'static', show: 'true'});
 };
 
-// to be used as a callback for the rails event "ajax:complete"
+// callback for event "ajax:complete"
 var my_ajax_complete = function(target, xhr, status) {
   console.log("handling ajax:complete");
-
-  jQuery('#ajax_spinner_modal').modal('hide');
-
 };
 
-// to be used as a callback for the rails event "ajax:error"
+// callback for event "ajax:error"
 var my_ajax_error = function(target, xhr, status, error) {
   console.log("handling ajax:error");
-
 };
 
-// to be used as a callback for the rails event "ajax:success"
+// callback for event "ajax:success"
 var my_ajax_success = function(target, data, status, xhr) {
   console.log("handling ajax:success");
   var element = jQuery(target);
-  
+
   var target_element = jQuery("#results");
 
   if( target_element ) {
 
-    var result = jQuery(xhr.responseText);
-
     // insert the response into the web page
-    target_element.append(result);
+    target_element.html(xhr.responseText);
 
   } else
     jQuery.error("my_ajax_success: Cannot find element '#results'" );
 
 };
 
-// datepicker components  
+// datepicker components
 jQuery(document)
   .delegate("[data-datepicker='true']", "focus", function() {
     dp = jQuery(this);
@@ -87,57 +71,39 @@ jQuery(document)
   });
 
 
-/* ******************************* FORM MASH UP *******************************/
-/* ******************************* FORM MASH UP *******************************/
-/* ******************************* FORM MASH UP *******************************/
+/* *************************** FORM MASH UP *******************************/
+/* *************************** FORM MASH UP *******************************/
+/* *************************** FORM MASH UP *******************************/
 
-// the target page use multiple forms.
-// we want to make just one query and pull back results from all those forms
+// combine multiple form posts in one call
 
-// use the native javascript FormData API to get all form inputs, particularly files
-// return false if at least one element with [required=true] is not filled out, true otherwise
+// use FormData API to get form inputs
+// group inputs by [data-form-mash]
 function build_request_body( form_data_parent, form_data, ajax_settings ) {
   if( form_data_parent && form_data ) {
     var form_inputs_selector = "select, textarea, input:not([type=submit])";
-    var form_inputs = form_data_parent.find( form_inputs_selector );
-    for( var e=0; e<form_inputs.length; e++ ) {
-      var dom_element = form_inputs.get(e);
-      var element = jQuery(dom_element);
-      var is_required = element.attr("required");
-      if( dom_element.getAttribute("type") == "file" ) {
-        if( is_required && dom_element.files.length==0 )
-          return false;
-        for( var f=0; f<dom_element.files.length; f++) {
-          console.log("FormData: appending a file: " + dom_element.name);
-          form_data.append( dom_element.name, dom_element.files[f] );
-        }
-      } else if( dom_element.getAttribute("type") == "hidden" ) {
-        console.log("FormData: appending typical form data: " + dom_element.name);
-        form_data.append( dom_element.name, dom_element.value );
-        // value_count++;  --- do not accoutn for hidden values; we want to determine if user entered anything in form
-      } else {
-        var v = jQuery.trim(dom_element.value);
-        if( v && v != "" ) {
-          console.log("FormData: appending typical form data: " + dom_element.name);
-          form_data.append( dom_element.name, v );
-        } else {
-          console.log("FormData: skipping blank form data: " + dom_element.name);
-          if( is_required )
-            return false;
-        }
-      }
-    }
-    // append the submit button value if this was a form
-    // the "ujs:submit-button" data was set in the "click.rails" event handler in rails.js (now named jquery_ujs.js)
-    if( form_data_parent.is("form") ) {
-      // get memoized value of the clicked submit button
-      var button = form_data_parent.data('ujs:submit-button');
-      if (button) {
-        console.log("FormData: appending submit button data: " + button.name);
-        form_data.append( button.name, button.value );
-        form_data_parent.data('ujs:submit-button', null);
-      }
-    }
+    var form_mashes = form_data_parent.find( "[data-form-mash]" );
+    for( var m=0; m<form_mashes.length; m++ ) {
+    	var form_inputs = jQuery(form_mashes[m]).find(form_inputs_selector);
+       	form_data.append( "form-mash-url[" + m + "]", jQuery(form_mashes[m]).attr("data-form-mash"));
+    	for( var e=0; e<form_inputs.length; e++ ) {
+      		var dom_element = form_inputs.get(e);
+      		var element = jQuery(dom_element);
+      		var is_required = element.attr("required");
+      		if( dom_element.getAttribute("type") == "hidden" ) {
+        		form_data.append( dom_element.name, dom_element.value );
+      		} else {
+        		var v = jQuery.trim(dom_element.value);
+        		if( v && v != "" ) {
+          			form_data.append( dom_element.name, v );
+        		} else {
+          			console.log("FormData: skipping blank form data: " + dom_element.name);
+          			if( is_required )
+            			return false;
+        		}
+      		}
+    	}
+	}
     // set data
     ajax_settings["data"] = form_data;
     ajax_settings["type"] = "POST";
@@ -151,8 +117,8 @@ function build_request_body( form_data_parent, form_data, ajax_settings ) {
 var ajax_form_submit = function(element, url, method, form_data) {
   console.log("submit: { url:'"+ url +"' }");
   jQuery.ajax({
-    url: 'form_mash.pl',
-    type: 'POST',
+    url: url,
+    type: method,
     data: form_data,
     processData: false,
     contentType: false,
@@ -176,16 +142,16 @@ var ajax_form_submit = function(element, url, method, form_data) {
       if (settings.dataType === undefined) {
         xhr.setRequestHeader('accept', '*/*;q=0.5, ' + settings.accepts.script);
       }
-      element.trigger('ajax:beforeSend', [xhr, settings]);
+      my_ajax_beforeSend(element, xhr, settings);
     },
     success: function(data, status, xhr) {
-      element.trigger('ajax:success', [data, status, xhr]);
+      my_ajax_success(element, data, status, xhr);
     },
     complete: function(xhr, status) {
-      element.trigger('ajax:complete', [xhr, status]);
+      my_ajax_complete(element, xhr, status);
     },
     error: function(xhr, status, error) {
-      element.trigger('ajax:error', [xhr, status, error]);
+      my_ajax_error(element, xhr, status, error);
     }
   });
 }
@@ -197,40 +163,32 @@ var ajax_form_submit = function(element, url, method, form_data) {
 var form_mash = function(target) {
 
 	console.log("form_mash");
-  var form = jQuery(target);
-  var form_data = new FormData();
-  if( form_data ) {
+	var form = jQuery(target);
+	var form_data = new FormData();
+	if( form_data ) {
+		settings = {};
+		build_request_body( form, form_data, settings );
 
-    settings = {};
-    build_request_body( form, form_data, settings );
-      
-	// searchCaseNo.cfm [caseNo]
-	// searchTypeRange.cfm [courtType, beginMonth, beginYear, endMonth, endYear]
-	// searchAuthor.cfm [authorID]
-	// searchCircuitJudge.cfm [circuitJudge]
-	// searchFamilyJudge.cfm [familyJudge]
-	// searchMasterJudge.cfm [masterJudge]
-	// /CFsearchSOLR/allPubOpinionsSearchResults.cfm [opinionSearchCriteria]
+		// searchCaseNo.cfm [caseNo]
+		// searchTypeRange.cfm [courtType, beginMonth, beginYear, endMonth, endYear]
+		// searchAuthor.cfm [authorID]
+		// searchCircuitJudge.cfm [circuitJudge]
+		// searchFamilyJudge.cfm [familyJudge]
+		// searchMasterJudge.cfm [masterJudge]
+		// /CFsearchSOLR/allPubOpinionsSearchResults.cfm [opinionSearchCriteria]
 
-	var mashes = form.find( "[data-form-mash]" );
-	console.log("mash count:" + mashes.length);
-	for( var e=0; e<mashes.length; e++ ) {
-		var mash = mashes[e];
-		var action = jQuery(mashes[e]).data("form-mash");
-		var method = "POST";
-	    	ajax_form_submit( form, action, method, form_data );
-	}
-	
-  } else {
-    jQuery.error("You browser does not support the FormData API. Try another web browser.");
-    xhr.abort();
-  }
+	    ajax_form_submit( form, "/cgi-bin/form_mash.pl", "POST", form_data );
 
-  return false;
+  	} else {
+    	jQuery.error("Your browser does not support the FormData API. Try another web browser.");
+    	xhr.abort();
+  	}
+
+	return false;
 }
 
 jQuery(document)
-  .delegate("form[data-trigger=form_mash]", "submit", function() {
-    form_mash(this);
-    return false;
-  });
+	.delegate("form[data-trigger=form_mash]", "submit", function() {
+    	form_mash(this);
+    	return false;
+  	});
